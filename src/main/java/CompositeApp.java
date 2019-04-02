a7e36dcb5adc3a18235a093ad3c02150b65bd0c1
Ivan <ivan_pylnev@mail>
2019-04-02 22:32:10 +0300

    add Composite

---
+++ src/main/java/CompositeApp.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeApp {
+    public static void main(String[] args) {
+        Shape square = new Square();
+        Shape triangle = new Triangle();
+        Shape sq = new Square();
+        Shape tr = new Triangle();
+
+        Composite composite = new Composite();
+        composite.addComponent(square);
+        composite.addComponent(triangle);
+
+        Composite compositeNext = new Composite();
+        compositeNext.addComponent(sq);
+        compositeNext.addComponent(tr);
+        composite.addComponent(compositeNext);
+        composite.draw();
+    }
+}
+
+interface Shape {
+    void draw();
+}
+
+class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Квадрат");
+    }
+}
+
+class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Круг");
+    }
+}
+
+class Triangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Треугольник");
+    }
+}
+
+class Composite implements Shape {
+    private List<Shape> components = new ArrayList<>();
+
+    public void addComponent(Shape component) {
+        this.components.add(component);
+    }
+
+
+    public void removeComponent(Shape component) {
+        this.components.remove(component);
+    }
+
+    //данным методом мы дойдем до самого глубоко уровня будь
+    // то компонент(NOde) или какой либо композит в котором есть своя нода
+    @Override
+    public void draw() {
+        for(Shape component :components){
+            component.draw();
+        }
+    }
+}
+
